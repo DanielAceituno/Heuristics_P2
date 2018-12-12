5625567624026a7a75c92feb94993bb5058e3d25
Daniel Aceituno <44235977+DanielAceituno@users.noreply.github.com>
2018-12-12 12:13:05 +0100

    Placement of Al and Snakes
    Still missing creation of output file and writing the result on it.

--- Solver.java
+++ Solver.java
@@ -75,10 +75,13 @@ public class Solver{
 
     //Proceeding to the phase of creating variables for the SAT problem.
 
+    int numSnakes=Integer.parseInt(args[1]);
+    int numberOfParallelGrids=1+numSnakes;
     //SI NO ENTIENDES BIEN ESTA PARTE ECHA UN OJO AL SEGURIDAD.JAVA, ES EL MISMO PROCESO.
-    BooleanVar [] allVariables=new BooleanVar[numRows*numCol+filledTiles];
+    BooleanVar [] allVariables=new BooleanVar[numberOfParallelGrids*numRows*numCol+filledTiles];
 
-    int [] literals=new int [numRows*numCol+filledTiles];
+
+    int [] literals=new int [numberOfParallelGrids*numRows*numCol+filledTiles];
 
     for(int i=0;i<numRows*numCol;i++){
       allVariables[i]=new BooleanVar(store, "Al is placed in position "+i+" of the map.");
@@ -92,7 +95,28 @@ public class Solver{
       literals [numCol*numRows+j] = satWrapper.cpVarToBoolVar(allVariables[numCol*numRows+j], 1, true);
     }
 
-    addAltoMap(satWrapper, literals, allRows); //Call to function to place Al.
+    for(int s=0;s<numSnakes;s++){
+      for(int k=0;k<numRows*numCol;k++){
+        allVariables[numRows*numCol+filledTiles+k+(s*numRows*numCol)]=new BooleanVar(store, "Snake"+(s+1)+" is placed in position "+k+" of the map.");
+        satWrapper.register(allVariables[numRows*numCol+filledTiles+k+(s*numRows*numCol)]);
+        literals [numRows*numCol+filledTiles+k+(s*numRows*numCol)] = satWrapper.cpVarToBoolVar(allVariables[numRows*numCol+filledTiles+k+(s*numRows*numCol)], 1, true);
+      }
+      addElementToMap(satWrapper, literals, allRows, (1+s)*numCol*numRows+filledTiles);
+      snakesAndAl(satWrapper, literals, (1+s)*numCol*numRows+filledTiles, numRows, numCol);
+    }
+
+    for(int t=0;t<numSnakes;t++){
+      for(int u=0;u<numSnakes;u++){
+        if(t!=u){
+          snakesAndSnakes(satWrapper, literals, t*numCol*numRows+filledTiles, u*numCol*numRows+filledTiles, numRows, numCol);
+        }
+      }
+    }
+
+    addElementToMap(satWrapper, literals, allRows, 0); //Call to function to place Al.
+    addFilledTiles(satWrapper, literals, allRows); //Call to function to make it impossible to place an element in an occupied tile.
+
+
 
     Search<BooleanVar> search = new DepthFirstSearch<BooleanVar>();
   SelectChoicePoint<BooleanVar> select = new SimpleSelect<BooleanVar>(allVariables,
@@ -100,48 +124,95 @@ public class Solver{
   Boolean result = search.labeling(store, select);
 
   System.out.println("Number of cells in the map:"+numCol*numRows);
+  System.out.println("Your user input is: "+args[1]);
   }
 
 
 
-  public static void addAltoMap(SatWrapper satWrapper, int literals[], String allRows[]){
+  public static void addElementToMap(SatWrapper satWrapper, int literals[], String allRows[], int offset){
     int k=0;
-    for(int i=0;i<allRows.length;i++){ //This part will make sure that only one or no Al can exist at the same time.
+    for(int i=0;i<allRows.length;i++){ //This part will make sure that only one or no Al or Snake can exist at the same time.
       for(int j=0;j<allRows.length;j++){ //(-a1 v -a2)^(-a1 v -a3)...
         if(i!=j){
           IntVec clause = new IntVec(satWrapper.pool);
-          int auxVal1=literals[i];
-          int auxVal2=literals[j];
+          int auxVal1=literals[i+offset];
+          int auxVal2=literals[j+offset];
       		clause.add(-auxVal1);
       		clause.add(-auxVal2);
       		satWrapper.addModelClause(clause.toArray());
         }
       }
     }
-    IntVec clause2 = new IntVec(satWrapper.pool); //This part will force that there is at least one tile containing Al.
+    IntVec clause2 = new IntVec(satWrapper.pool); //This part will force that there is at least one tile containing Al or a Snake.
     for(int l=0;l<allRows.length;l++){ //(a1 v a2 v a3 v a4...)
-      int aux=literals[l];
+      int aux=literals[l+offset];
       clause2.add(aux);
     }
     satWrapper.addModelClause(clause2.toArray());
 
-    //Adding clauses so that Al cannot be placed in any kind of obstacle.
+    //Adding clauses so that Al or Snake cannot be placed in any kind of obstacle.
     int countingFilled=0;
     for(int w=0;w<allRows.length;w++){
+
       if(!allRows[w].equals(" ")){
         IntVec clause3 = new IntVec(satWrapper.pool);
         int auxVal1=literals[allRows.length+countingFilled];
-        int auxVal2=literals[w];
+        int auxVal2=literals[w+offset];
         clause3.add(-auxVal1);
         clause3.add(-auxVal2);
         satWrapper.addModelClause(clause3.toArray());
 
-        //This will force the existance of an obstacle to be true if there is any:
-        IntVec clause4 = new IntVec(satWrapper.pool);
-        clause4.add(literals[allRows.length+countingFilled]);
-        satWrapper.addModelClause(clause4.toArray());
-
         countingFilled++;
+      }
+    }
+  }
+
+  public static void addFilledTiles(SatWrapper satWrapper, int literals[], String allRows[]){
+    //This will force the existance of an obstacle to be true if there is any:
+    int filledTiles=0;
+    for(int i=0; i<allRows.length;i++){
+      if(!allRows[i].equals(" ")){
+        IntVec clause = new IntVec(satWrapper.pool);
+        clause.add(literals[allRows.length+filledTiles]);
+        satWrapper.addModelClause(clause.toArray());
+
+        filledTiles++;
+      }
+    }
+  }
+
+  public static void snakesAndAl(SatWrapper satWrapper, int literals[], int offset, int numRows, int numCol){
+      for(int i=0;i<numRows;i++){
+        for(int j=0;j<numCol;j++){
+          //Part for not generating snakes in the same row as Al.
+          for(int k=0;k<numCol;k++){
+            IntVec clause = new IntVec(satWrapper.pool);
+            clause.add(-literals[i*numCol+j]);
+            clause.add(-literals[i*numCol+k+offset]);
+            satWrapper.addModelClause(clause.toArray());
+          }
+          //Part for not generating snakes in the same column as Al.
+          for(int l=0;l<numRows;l++){
+            IntVec clause2 = new IntVec(satWrapper.pool);
+            clause2.add(-literals[i*numCol+j]);
+            clause2.add(-literals[l*numCol+j+offset]);
+            satWrapper.addModelClause(clause2.toArray());
+          }
+        }
+      }
+  }
+
+  public static void snakesAndSnakes(SatWrapper satWrapper, int literals[], int offset1, int offset2, int numRows, int numCol){
+
+    for(int i=0;i<numRows;i++){
+      for(int j=0;j<numCol;j++){
+        //Part for not generating snakes in the same row as Al.
+        for(int k=0;k<numCol;k++){
+          IntVec clause = new IntVec(satWrapper.pool);
+          clause.add(-literals[i*numCol+j+offset1]);
+          clause.add(-literals[i*numCol+k+offset2]);
+          satWrapper.addModelClause(clause.toArray());
+        }
       }
     }
   }
