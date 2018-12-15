f0174755f503231027d6c73db6d28769f51242cd
Daniel Aceituno <44235977+DanielAceituno@users.noreply.github.com>
2018-12-15 19:52:42 +0100

    Final working version (v0.9)
    Still lacks some comments and writing the output file. Does the placing of elements perfectly.

--- Solver.java
+++ Solver.java
@@ -55,33 +55,16 @@ public class Solver{
       }
     }
 
-    //COUNTING ALL THE TILES THAT ARE NOT FREE:
-    int filledTiles=0;
-    for(int z=0;z<numCol*numRows;z++){
-      if(!allRows[z].equals(" ")){
-        filledTiles++;
-      }
-    }
-
-    /*for(int u=0;u<allRows.length;u++){ PARA PRINTEAR ALLROWS
-      System.out.print(allRows[u]);
-    } */
-
-    /*for(int o=0;o<numRows;o++){ PARA PRINTEAR MAPCONTENT
-      for(int p=0;p<numCol;p++){
-        System.out.print(mapContent[o][p]);
-      }
-    } */
-
     //Proceeding to the phase of creating variables for the SAT problem.
 
     int numSnakes=Integer.parseInt(args[1]);
     int numberOfParallelGrids=1+numSnakes;
-    //SI NO ENTIENDES BIEN ESTA PARTE ECHA UN OJO AL SEGURIDAD.JAVA, ES EL MISMO PROCESO.
-    BooleanVar [] allVariables=new BooleanVar[numberOfParallelGrids*numRows*numCol+filledTiles];
 
 
-    int [] literals=new int [numberOfParallelGrids*numRows*numCol+filledTiles];
+    BooleanVar [] allVariables=new BooleanVar[numberOfParallelGrids*numRows*numCol];
+
+    int [] literals=new int [numberOfParallelGrids*numRows*numCol];
+
 
     for(int i=0;i<numRows*numCol;i++){
       allVariables[i]=new BooleanVar(store, "Al is placed in position "+i+" of the map.");
@@ -89,42 +72,60 @@ public class Solver{
       literals [i] = satWrapper.cpVarToBoolVar(allVariables[i], 1, true);
     }
 
-    for(int j=0;j<filledTiles;j++){
-      allVariables[numRows*numCol+j]=new BooleanVar(store, "There is an obstacle (with ID "+j+")");
-      satWrapper.register(allVariables[numCol*numRows+j]);
-      literals [numCol*numRows+j] = satWrapper.cpVarToBoolVar(allVariables[numCol*numRows+j], 1, true);
-    }
-
     for(int s=0;s<numSnakes;s++){
       for(int k=0;k<numRows*numCol;k++){
-        allVariables[numRows*numCol+filledTiles+k+(s*numRows*numCol)]=new BooleanVar(store, "Snake"+(s+1)+" is placed in position "+k+" of the map.");
-        satWrapper.register(allVariables[numRows*numCol+filledTiles+k+(s*numRows*numCol)]);
-        literals [numRows*numCol+filledTiles+k+(s*numRows*numCol)] = satWrapper.cpVarToBoolVar(allVariables[numRows*numCol+filledTiles+k+(s*numRows*numCol)], 1, true);
+        allVariables[k+((s+1)*numRows*numCol)]=new BooleanVar(store, "Snake"+(s+1)+" is placed in position "+k+" of the map.");
+        satWrapper.register(allVariables[k+((s+1)*numRows*numCol)]);
+        literals [k+((s+1)*numRows*numCol)] = satWrapper.cpVarToBoolVar(allVariables[k+((s+1)*numRows*numCol)], 1, true);
       }
-      addElementToMap(satWrapper, literals, allRows, (1+s)*numCol*numRows+filledTiles);
-      snakesAndAl(satWrapper, literals, (1+s)*numCol*numRows+filledTiles, numRows, numCol);
+      addElementToMap(satWrapper, literals, allRows, (s+1)*numCol*numRows);
+      snakesAndAl(satWrapper, literals, (s+1)*numCol*numRows, numRows, numCol);
     }
 
-    for(int t=0;t<numSnakes;t++){
-      for(int u=0;u<numSnakes;u++){
+    for(int t=1;t<=numSnakes;t++){
+      for(int u=1;u<=numSnakes;u++){
         if(t!=u){
-          snakesAndSnakes(satWrapper, literals, t*numCol*numRows+filledTiles, u*numCol*numRows+filledTiles, numRows, numCol);
+          snakesAndSnakes(satWrapper, literals, t*numCol*numRows, u*numCol*numRows, numRows, numCol);
         }
       }
     }
 
     addElementToMap(satWrapper, literals, allRows, 0); //Call to function to place Al.
-    addFilledTiles(satWrapper, literals, allRows); //Call to function to make it impossible to place an element in an occupied tile.
-
 
 
     Search<BooleanVar> search = new DepthFirstSearch<BooleanVar>();
-  SelectChoicePoint<BooleanVar> select = new SimpleSelect<BooleanVar>(allVariables,
-             new SmallestDomain<BooleanVar>(), new IndomainMin<BooleanVar>());
-  Boolean result = search.labeling(store, select);
+    SelectChoicePoint<BooleanVar> select = new SimpleSelect<BooleanVar>(allVariables, new SmallestDomain<BooleanVar>(), new IndomainMin<BooleanVar>());
+    Boolean result = search.labeling(store, select);
 
-  System.out.println("Number of cells in the map:"+numCol*numRows);
-  System.out.println("Your user input is: "+args[1]);
+    //System.out.println("Number of cells in the map:"+numCol*numRows);
+    //System.out.println("Your user input is: "+args[1]);
+
+    for(int n=0;n<numCol*numRows;n++){
+      if(allVariables[n].dom().value()==1){
+        allRows[n]="A";
+      }
+    }
+
+    for(int p=0;p<numSnakes;p++){
+      for(int q=0;q<numCol*numRows;q++){
+        if(allVariables[numRows*numCol+q+(p*numRows*numCol)].dom().value()==1){
+          allRows[q]="S";
+        }
+      }
+    }
+
+
+    //For printing the result once finished.
+    int elemCounter=0;
+    for(int o=0;o<allRows.length;o++){
+      System.out.print(allRows[o]);
+      elemCounter++;
+      if(elemCounter>=numCol){
+        System.out.println("");
+        elemCounter=0;
+      }
+    }
+
   }
 
 
@@ -135,48 +136,26 @@ public class Solver{
       for(int j=0;j<allRows.length;j++){ //(-a1 v -a2)^(-a1 v -a3)...
         if(i!=j){
           IntVec clause = new IntVec(satWrapper.pool);
-          int auxVal1=literals[i+offset];
-          int auxVal2=literals[j+offset];
-      		clause.add(-auxVal1);
-      		clause.add(-auxVal2);
+      		clause.add(-literals[i+offset]);
+      		clause.add(-literals[j+offset]);
       		satWrapper.addModelClause(clause.toArray());
         }
       }
     }
     IntVec clause2 = new IntVec(satWrapper.pool); //This part will force that there is at least one tile containing Al or a Snake.
     for(int l=0;l<allRows.length;l++){ //(a1 v a2 v a3 v a4...)
-      int aux=literals[l+offset];
-      clause2.add(aux);
+      clause2.add(literals[l+offset]);
     }
     satWrapper.addModelClause(clause2.toArray());
 
     //Adding clauses so that Al or Snake cannot be placed in any kind of obstacle.
-    int countingFilled=0;
     for(int w=0;w<allRows.length;w++){
 
       if(!allRows[w].equals(" ")){
         IntVec clause3 = new IntVec(satWrapper.pool);
-        int auxVal1=literals[allRows.length+countingFilled];
-        int auxVal2=literals[w+offset];
-        clause3.add(-auxVal1);
-        clause3.add(-auxVal2);
+        clause3.add(-literals[w+offset]);
         satWrapper.addModelClause(clause3.toArray());
 
-        countingFilled++;
-      }
-    }
-  }
-
-  public static void addFilledTiles(SatWrapper satWrapper, int literals[], String allRows[]){
-    //This will force the existance of an obstacle to be true if there is any:
-    int filledTiles=0;
-    for(int i=0; i<allRows.length;i++){
-      if(!allRows[i].equals(" ")){
-        IntVec clause = new IntVec(satWrapper.pool);
-        clause.add(literals[allRows.length+filledTiles]);
-        satWrapper.addModelClause(clause.toArray());
-
-        filledTiles++;
       }
     }
   }
